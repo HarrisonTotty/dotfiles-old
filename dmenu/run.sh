a7a297702e2777ef810280a04d3cdc1bf9f9d1b4
Harrison Totty <harrisongtotty@gmail.com>
2016-04-26 13:29:09 -0400

    Update run.sh

--- dmenu/run.sh
+++ dmenu/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # The following command is run when the appropriate keyboard shortcut is pressed to show dmenu.
-# Requires aur/dmenu-xft-wxy-mouse
+# Requires aur/dmenu-xft-wxy-mouse and the uushi font.
 
 dmenu_run -l 0 -p 'Launch >' -nb '#444444' -nf '#f9f9f9' -sb '#d64937' -sf '#f9f9f9' -fn 'uushi-9' -w 1366 -x 0 -y 4
